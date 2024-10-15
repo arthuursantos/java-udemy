b2ea7a977a321f8e4a5747447e8447bf90809be1
Arthur Santos <arthur.saantos20@gmail.com>
2024-10-15 17:16:18 -0300

    Implementa a entidade User e seu resource

---
+++ udemy-java21-spring/project/src/main/java/com/javaudemy/project/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.javaudemy.project.resources;
+
+import com.javaudemy.project.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        return ResponseEntity.ok().body(new User(1, "Arthur Santos", "arthur@gmail.com", "19999356768", "vaicorinthians"));
+    }
+
+}
