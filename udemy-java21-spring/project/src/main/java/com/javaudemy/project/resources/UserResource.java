0c456a6098193a0258762baed7aa4eb415b30501
Arthur Santos <arthur.saantos20@gmail.com>
2024-10-17 17:25:44 -0300

    Implementa a camada de serviço e o registro dos componentes

--- udemy-java21-spring/project/src/main/java/com/javaudemy/project/resources/UserResource.java
+++ udemy-java21-spring/project/src/main/java/com/javaudemy/project/resources/UserResource.java
@@ -1,18 +1,31 @@
 package com.javaudemy.project.resources;
 
 import com.javaudemy.project.entities.User;
+import com.javaudemy.project.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        return ResponseEntity.ok().body(new User("Gustavo Henrique", "gustavo@hotmail.com", "19999356768", "saltaofc"));
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable int id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 
 }
---
+++ udemy-java21-spring/project/src/main/java/com/javaudemy/project/services/UserService.java
@@ -0,0 +1,26 @@
+package com.javaudemy.project.services;
+
+import com.javaudemy.project.entities.User;
+import com.javaudemy.project.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(int id) {
+        Optional<User> user = repository.findById(id);
+        return user.get();
+    }
+
+}
