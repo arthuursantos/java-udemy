550fcddfefc1bc830eaffddec0655b123cadde1d
Arthur Santos <arthur.saantos20@gmail.com>
2024-10-24 20:44:07 -0300

    Implementar a inserção de User

--- udemy-java21-spring/project/src/main/java/com/javaudemy/project/resources/UserResource.java
+++ udemy-java21-spring/project/src/main/java/com/javaudemy/project/resources/UserResource.java
@@ -5,7 +5,9 @@ import com.javaudemy.project.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +28,13 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable int id) {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        User obj = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 }
