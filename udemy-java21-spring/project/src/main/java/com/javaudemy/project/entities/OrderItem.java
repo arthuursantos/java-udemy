fd3b7eb7c015a77f7b68f9dfe870ee7a225c853e
Arthur Santos <arthur.saantos20@gmail.com>
2024-10-24 17:35:21 -0300

    Implementar os métodos getSubtotal e getTotal

--- udemy-java21-spring/project/src/main/java/com/javaudemy/project/entities/OrderItem.java
+++ udemy-java21-spring/project/src/main/java/com/javaudemy/project/entities/OrderItem.java
@@ -60,4 +60,9 @@ public class OrderItem implements Serializable {
     public void setPrice(Double price) {
         this.price = price;
     }
+
+    public Double getSubtotal() {
+        return price*quantity;
+    }
+
 }
